8cfc4a7d6d00b550131dec3fc8cc01088343acf4
Joey Chinoski <joeychin01@yahoo.com>
2020-06-16 18:21:20 +0000

    Initial version of FindMeetingQuery

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,68 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> meetings = new ArrayList<TimeRange>();
+    ArrayList<Event> eventTimes = new ArrayList<Event>(events);
+    ArrayList<String> requestAttendees = new ArrayList(request.getAttendees());
+    Collections.sort(eventTimes, Comparator.comparing(Event::getWhen, TimeRange.ORDER_BY_START));
+    long duration = request.getDuration();
+    if(duration > 23 * 60 + 59){
+      return meetings;
+    }
+
+    ArrayList<TimeRange> busyTimes = new ArrayList<TimeRange>();
+    for (Event event: eventTimes){
+      if(attendentAttending(event.getAttendees(), requestAttendees)){
+        if(busyTimes.isEmpty()){
+          busyTimes.add(event.getWhen());
+        } else if(busyTimes.get(busyTimes.size() - 1).overlaps(event.getWhen())) {
+          busyTimes.set(busyTimes.size() - 1, 
+            TimeRange.fromStartEnd(busyTimes.get(busyTimes.size() - 1).start(),  event.getWhen().end(), false));
+        } else {
+          busyTimes.add(event.getWhen());
+        }
+      }
+    }
+
+    if(busyTimes.isEmpty()){
+      meetings.add(TimeRange.WHOLE_DAY);
+      return meetings;
+    }
+
+    if(busyTimes.get(0).start() >= duration){
+      meetings.add(TimeRange.fromStartDuration(0, busyTimes.get(0).start()));
+    }
+    for(int i = 0; i < busyTimes.size() - 1; i++){
+      if(busyTimes.get(i + 1).start() - busyTimes.get(i).end() >= duration){
+        meetings.add(TimeRange.fromStartEnd(busyTimes.get(i).end(), busyTimes.get(i + 1).start(), false));
+      }
+    }
+    if((((23 * 60) + 59) - busyTimes.get(busyTimes.size() - 1).end()) >= duration){
+      meetings.add(TimeRange.fromStartEnd(busyTimes.get(busyTimes.size() - 1).end(), 24 * 60, false));
+    }
+
+    return meetings;
+  }
+
+
+
+  //returns true if an attendee is in the event attendees
+  private boolean attendentAttending(Set<String> eventAttendees, ArrayList<String> attendees) {
+    for(int i = 0; i < attendees.size(); i++){
+      if(eventAttendees.contains(attendees.get(i))){
+        return true;
+      }
+    }
+    return false;
   }
 }
